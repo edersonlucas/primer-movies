9f0643fd68f0a280f016a371da9cca98f89b7a19
Ederson Lucas <edersonlucas@outlook.com.br>
2023-05-04 17:47:40 -0300

    style: leave the header fixed

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -5,8 +5,8 @@ import logo from '../assets/img/logo.svg';
 
 export default function Header() {
   return (
-    <header className="flex h-16 w-full justify-center bg-white p-4 md:justify-start">
-      <Link href="/">
+    <header className="fixed top-0 z-50 flex h-16 w-full justify-center bg-white p-4 md:justify-start">
+      <Link className="my-auto" href="/">
         <Image
           priority={true}
           className="md:mx-10"
