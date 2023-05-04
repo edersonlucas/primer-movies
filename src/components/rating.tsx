5ccf6f1266e62a0abee18f753a6f3b7867862d2b
Ederson Lucas <edersonlucas@outlook.com.br>
2023-05-04 17:51:30 -0300

    style: increase the width of the rating component

--- src/components/rating.tsx
+++ src/components/rating.tsx
@@ -6,8 +6,8 @@ interface RatingProps {
 
 export default function Rating({ rating }: RatingProps) {
   return (
-    <span className="flex max-w-[5rem] items-center gap-1 rounded-full bg-primary px-3 py-1 opacity-90">
-      <Star size={15} weight="fill" />
+    <span className="flex max-w-[5.3rem] items-center gap-1 rounded-full bg-primary px-3 py-1 opacity-90">
+      <Star size={13} weight="fill" />
       <p className="text-xs font-semibold">{rating}/10</p>
     </span>
   );
